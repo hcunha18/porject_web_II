4caa723504a4a5a83cb60c54310509a4996c9a9e
hcunha18 <humbertomottadacunha@gmail.com>
2024-03-23 11:58:32 -0300

    Divider

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,14 @@
+import Cardevent from '../../components/Cardevent';
 import Navbar from '../../components/Navbar';
-
+import Divider from '@mui/material/Divider';
 
 export default function Home() {
     return (
-        < Navbar />
+        <>
+            < Navbar />
+            <Divider sx={{marginTop: 10, marginBottom: 10}}/>
+            
+            <Cardevent />
+        </>
     );
 }
