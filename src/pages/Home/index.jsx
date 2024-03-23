4de808244a035704bf7ce98c28f1675c3c4091f9
Vinicius Lima <viniciuslima99@live.com>
2024-03-23 12:30:15 -0300

    titulo_sem_estilo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { Container, Typography } from '@mui/material';
 import Cardevent from '../../components/Cardevent';
 import Navbar from '../../components/Navbar';
 import Divider from '@mui/material/Divider';
@@ -6,6 +7,10 @@ export default function Home() {
     return (
         <>
             < Navbar />
+            <Container>
+                <Typography>Divulgue seu evento</Typography>
+            </Container>
+            
             <Divider sx={{marginTop: 10, marginBottom: 10}}/>
             
             <Cardevent />
